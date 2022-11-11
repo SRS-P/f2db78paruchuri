31f4e6c90b968d165f159f8b7400ffcc4eb694fe
SRS-P <SRS-P@users.noreply.github.com>
2022-11-11 14:31:33 -0600

    Update house.js

--- controllers/house.js
+++ controllers/house.js
@@ -12,10 +12,7 @@ exports.house_list = async function(req, res) {
     }   
 }; 
  
-// for a specific house. 
-exports.house_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: house detail: ' + req.params.id); 
-}; 
+
  
 // Handle house create on POST. 
 exports.house_create_post = async function(req, res) { 
@@ -24,7 +21,7 @@ exports.house_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"house_type":"goat", "cost":12, "size":"large"} 
 
     document.house_model = req.body.house_model; 
     document.house_age = req.body.house_age; 
@@ -61,3 +58,34 @@ exports.house_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
+// for a specific house.
+exports.house_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await house.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+// Handle House update form on PUT. 
+exports.house_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await house.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.house_model) toUpdate.house_model = req.body.house_model; 
+        if(req.body.house_age) toUpdate.house_age = req.body.house_age; 
+        if(req.body.house_height) toUpdate.house_height = req.body.house_height; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
