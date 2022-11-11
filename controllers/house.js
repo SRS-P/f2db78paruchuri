598977f3b363b939b7caed278fead6a95e58ae5f
SRS-P <SRS-P@users.noreply.github.com>
2022-11-11 13:18:10 -0600

    Create house.js

---
+++ controllers/house.js
@@ -0,0 +1,63 @@
+var house = require('../models/house'); 
+ 
+// List of all houses 
+exports.house_list = async function(req, res) { 
+    try{ 
+        thehouses = await house.find(); 
+        res.send(thehouses); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ 
+// for a specific house. 
+exports.house_detail = function(req, res) { 
+    res.send('NOT IMPLEMENTED: house detail: ' + req.params.id); 
+}; 
+ 
+// Handle house create on POST. 
+exports.house_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new house(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+
+    document.house_model = req.body.house_model; 
+    document.house_age = req.body.house_age; 
+    document.house_height = req.body.house_height; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }  
+}; 
+ 
+// Handle house delete form on DELETE. 
+exports.house_delete = function(req, res) { 
+    res.send('NOT IMPLEMENTED: house delete DELETE ' + req.params.id); 
+}; 
+ 
+// Handle house update form on PUT. 
+exports.house_update_put = function(req, res) { 
+    res.send('NOT IMPLEMENTED: house update PUT' + req.params.id); 
+}; 
+
+// VIEWS 
+// Handle a show all view 
+exports.house_view_all_Page = async function(req, res) { 
+    try{ 
+        thehouses = await house.find(); 
+        res.render('houses', { title: 'house Search Results', results: thehouses }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};
