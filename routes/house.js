4cb172882d4243494e9c587fae5b493d6b2b6a26
SRS-P <SRS-P@users.noreply.github.com>
2022-11-30 21:15:40 -0600

    Update house.js

--- routes/house.js
+++ routes/house.js
@@ -1,24 +1,30 @@
 var express = require('express');
-
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 var router = express.Router();
 var house_controller = require('../controllers/house'); 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('house', { title: 'Search Results - house' });
-});
+router.get('/', house_controller.house_view_all_Page);
 
 /* GET detail house page */ 
-router.get('/detail',house_controller.house_view_one_Page); 
+router.get('/detail',secured,house_controller.house_view_one_Page); 
 
 /* GET create house page */ 
 router.get('/create', house_controller.house_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', house_controller.house_update_Page); 
+router.get('/update',secured, house_controller.house_update_Page); 
 
 /* GET delete house page */ 
-router.get('/delete', house_controller.house_delete_Page); 
+router.get('/delete',secured, house_controller.house_delete_Page); 
  
  
 
