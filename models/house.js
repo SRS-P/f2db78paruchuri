4000b02d484982554d5f69f8a93be18ccd4a8cdf
SRS-P <SRS-P@users.noreply.github.com>
2022-11-30 21:13:49 -0600

    Update house.js

--- models/house.js
+++ models/house.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose") 
 const houseSchema = mongoose.Schema({ 
-    house_model: String, 
-    house_age: Number, 
-    house_height: String 
+    house_model: String,
+    house_height: String,
+    house_age:{ 
+        type:Number, 
+    min:1,
+    max:70
+    }
+    
 }) 
  
 module.exports = mongoose.model("house", houseSchema)
